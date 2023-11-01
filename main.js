633b2563375f0f745ed1ef4228cd50e68d58c61f
Aspenleger15 <127303142+Aspenleger15@users.noreply.github.com>
2023-11-01 11:43:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 // Pom......................................................
-function tocaSomPom (){
+function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();   
 }
     
-document.querySelectorAll('.tecla')
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let
+
+while () {
+    listaDeTeclas[0].onclick = tocaSomPom; 
+}
+
+
 
 
 
